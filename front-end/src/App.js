1d94571f4bc1dff118da362baf7061202f35b4d6
Kohl Peterson <kohl.peterson@sap.com>
2020-10-24 09:56:31 -0700

    committing to update to master

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -9,17 +9,32 @@ function App() {
   useEffect(()=> {
     if (Object.keys(data).length === 0) {
       axios.post('http://localhost:3000/api/image').then(result => {
-      console.log(result);
-      setData(result);
+      setData(result.data);
     }).catch(err => {
-      console.log(err);
     });
+    } else {
+      console.log(data);
     }
   })
+
+  const renderData = item => {
+    if (!item || !item.value || !item.value.en) return null;
+    return(
+      <span>{item.value.en}</span>
+    );
+  }
 
   return (
     <div className="App">
       <Camera />
+      {
+      Object.keys(data).length !== 0 
+      ?
+      data.opts.variables.map(item => {
+        return renderData(item)})
+      : 
+      null
+      }
     </div>
   );
 }
