693df7ef0b5dbaca5c6bae831c8a6026f039a87f
Kohl Peterson <kohl.peterson@sap.com>
2020-10-23 18:46:35 -0700

    https issue fixing

---
+++ front-end/src/components/Camera.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import Camera from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
+import PropTypes from 'prop-types';
+
+const ImagePreview = ({ dataUri, isFullscreen }) => {
+  let classNameFullscreen = isFullscreen ? 'demo-image-preview-fullscreen' : '';
+
+  return (
+    <div className={'demo-image-preview ' + classNameFullscreen}>
+      <img src={dataUri} />
+    </div>
+  );
+};
+
+ImagePreview.propTypes = {
+  dataUri: PropTypes.string,
+  isFullscreen: PropTypes.bool
+};
+ 
+function MyCamera (props) {
+  const [dataUri, setDataUri] = useState('');
+ 
+  function handleTakePhotoAnimationDone (dataUri) {
+    console.log('takePhoto');
+    setDataUri(dataUri);
+  }
+ 
+  const isFullscreen = false;
+  return (
+    <div>
+      {
+        (dataUri)
+          ? <ImagePreview dataUri={dataUri}
+            isFullscreen={isFullscreen}
+          />
+          : <Camera onTakePhotoAnimationDone = {handleTakePhotoAnimationDone}
+            isFullscreen={isFullscreen}
+          />
+      }
+    </div>
+  );
+}
+ 
+export default MyCamera;
