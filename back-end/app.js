5da00c2855d14415362bdd4cfb4a8529a4516ac0
Timan Njenga Wamweya <njenga.wamweya@sap.com>
2020-10-24 12:08:05 -0700

    Encrypted api key and added try catch funcitonality

--- back-end/app.js
+++ back-end/app.js
@@ -4,7 +4,7 @@ const port = 3000
 
 const image = require('./routes/api/image');
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
@@ -20,49 +20,6 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
-
-//Below code calls Cloud Vision API and uses both object detection and label detection to fetch image tags
-async function detectimage(fileName) {
-    // Imports the Google Cloud client libraries
-    const vision = require('@google-cloud/vision');
-    const fs = require('fs');
-
-    // Creates a client
-    const client = new vision.ImageAnnotatorClient({
-        keyFilename: 'smart-recycle-12345-758a032defdf.json'
-    });
-
-    const request = {
-        image: { content: fs.readFileSync(fileName) },
-    };
-
-    //below part of the code fetches the predicted objects
-    const [result] = await client.objectLocalization(request);
-    var pred_flag = true
-
-    const objects = result.localizedObjectAnnotations;
-    //console.log(objects)
-    var arr_obj_det = [];
-    var lab_obj_det = '';
-    objects.forEach(object => {
-        arr_obj_det.push(object.name);
-        lab_obj_det = arr_obj_det.slice(1, 3).join(' ');
-        console.log(lab_obj_det);
-    });
-
-    //below code fetches the predicted labels
-    var arr_lab_det = [];
-    var lab_lab_det = '';
-    var newVar = await client.labelDetection(fileName);
-    const labels = newVar[0].labelAnnotations;
-    labels.forEach(label => {
-        arr_lab_det.push(label.description);
-        lab_lab_det = arr_lab_det.slice(1, 3).join(' ');
-    })
-    console.log(lab_lab_det);
-    final_string = lab_obj_det + " " + lab_lab_det;
-    console.log(`Search String is:${final_string}`);
-}
-
+const visionAPI = require('./visionAPI.js');
 const fileName = './beer-bottles.jpg';
-detectimage(fileName);
+visionAPI.detectimage(fileName);
---
+++ back-end/visionAPI.js
@@ -0,0 +1,70 @@
+module.exports = {
+    //Below code calls Cloud Vision API and uses both object detection and label detection to fetch image tags
+    detectimage: async function detectimage(fileName) {
+        // Imports the Google Cloud client libraries
+        const vision = require('@google-cloud/vision');
+        const fs = require('fs');
+        const cryptoJSON = require('crypto-json');
+
+        // Decrypts the writtenObject
+        let readFile;
+        try {
+            readFile = fs.readFileSync('writtenObject.json');
+            readFile = JSON.parse(readFile);
+            readFile = cryptoJSON.decrypt(readFile, 'SmartRecycle');
+        } catch (error) {
+            console.log(error);
+        }
+
+        // Creates a client
+        const client = new vision.ImageAnnotatorClient({
+            credentials: readFile
+        });
+        let request;
+        try {
+            request = {
+                image: { content: fs.readFileSync(fileName) },
+            };
+        } catch (error) {
+            console.log(error);
+        }
+
+        //below part of the code fetches the predicted objects
+        let result;
+        let objects = [];
+        var arr_obj_det = [];
+        var lab_obj_det = '';
+        try {
+            [result] = await client.objectLocalization(request);
+            objects.push(result.localizedObjectAnnotations);
+            objects.forEach(object => {
+                arr_obj_det.push(object.name);
+                lab_obj_det = arr_obj_det.slice(1, 3).join(' ');
+                console.log(lab_obj_det);
+            });
+        } catch (error) {
+            console.log(error);
+        }
+        var pred_flag = true
+
+        //below code fetches the predicted labels
+        var arr_lab_det = [];
+        var lab_lab_det = '';
+        let newVar;
+        let labels = [];
+        try {
+            newVar = await client.labelDetection(fileName);
+            labels = newVar[0].labelAnnotations;
+            labels.forEach(label => {
+                arr_lab_det.push(label.description);
+                lab_lab_det = arr_lab_det.slice(1, 3).join(' ');
+            })
+        } catch (error) {
+            console.log(error);
+        }
+
+        console.log(lab_lab_det);
+        final_string = lab_obj_det + " " + lab_lab_det;
+        console.log(`Search String is:${final_string}`);
+    }
+}
