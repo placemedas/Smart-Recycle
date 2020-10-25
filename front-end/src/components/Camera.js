9a030b425512cac87aa70917ad37dfefc1758490
Timan Njenga Wamweya <njenga.wamweya@sap.com>
2020-10-24 23:53:54 -0700

    added favicons

--- front-end/src/components/Camera.js
+++ front-end/src/components/Camera.js
@@ -3,7 +3,12 @@ import Camera, {FACING_MODES} from 'react-html5-camera-photo';
 import 'react-html5-camera-photo/build/css/index.css';
 import PropTypes from 'prop-types';
 import axios from 'axios';
-import { host } from '../config'
+import { host } from '../config';
+import './Card.css';
+import Paper from '@material-ui/core/Paper';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
 
 const ImagePreview = ({ dataUri, isFullscreen }) => {
   let classNameFullscreen = isFullscreen ? 'demo-image-preview-fullscreen' : '';
@@ -22,7 +27,7 @@ ImagePreview.propTypes = {
 
 class MyCamera extends React.Component {
   state = {
-
+    error: false
   }
 
   handleTakePhotoAnimationDone = (dataUri) => {
@@ -36,6 +41,37 @@ class MyCamera extends React.Component {
     this.props.setData({dataUri: dataUri});
   }
 
+  handleCameraError = (error) => {
+    if (error) {
+      this.setState({error: true})
+    };
+  }
+
+  displayCamera = (isFullscreen) => {
+    if (this.state.error) {
+      return (
+        <Paper className="bottomPaper" elevation={3}>
+          <Card className="bottomCard" variant="outlined">
+            <CardContent>
+              <Typography variant="h6" component="h4" style={{color: 'red'}}>
+              {"Please check your camera permissions and then reload"}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Paper>
+      )
+    } else {
+      return (
+        <Camera onTakePhotoAnimationDone={this.handleTakePhotoAnimationDone}
+              isFullscreen={isFullscreen}
+              isImageMirror ={window.matchMedia("(min-width: 568px)").matches}
+              idealFacingMode = {FACING_MODES.ENVIRONMENT}
+              onCameraError={this.handleCameraError}
+            />
+      )
+    }
+  }
+
   render() {
     const isFullscreen = false;
     return (
@@ -45,10 +81,7 @@ class MyCamera extends React.Component {
             ? <ImagePreview dataUri={this.props.dataUri}
               isFullscreen={isFullscreen}
             />
-            : <Camera onTakePhotoAnimationDone={this.handleTakePhotoAnimationDone}
-              isFullscreen={isFullscreen}
-              idealFacingMode = {FACING_MODES.ENVIRONMENT}
-            />
+            : this.displayCamera(isFullscreen)
         }
       </div>
     );
